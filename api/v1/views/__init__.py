e3a09049bd9de548291003577ca13443ed328e92
Void-Individual <otayomi@gmail.com>
2024-04-28 09:15:57 +0000

    Changed the app_views format

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,9 @@
 """The init module"""
 
 from flask import Blueprint
+
+app_views = Blueprint('index', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 #from api.v1.views.cities import *
-
-app_views = Blueprint('index', __name__, url_prefix='/api/v1')
