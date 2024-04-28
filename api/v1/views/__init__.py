0345ef04fa16cd61f94913a2d819d0d6e814700b
Void-Individual <otayomitv@gmail.com>
2024-04-29 00:28:36 +0100

    a pycodestyle error in views/init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,9 @@
 #!/user/bin/python3
 """The init module"""
 
-from flask import Blueprint
-
-app_views = Blueprint('index', __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+from flask import Blueprint
+
+app_views = Blueprint('index', __name__, url_prefix='/api/v1')
