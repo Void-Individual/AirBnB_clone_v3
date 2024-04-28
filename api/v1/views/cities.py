1371aa394683d9ca4002cb3911c4359c3164acec
Void-Individual <otayomitv@gmail.com>
2024-04-28 08:55:48 +0100

    some changes to cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -2,7 +2,7 @@
 """Module to handle the city objects"""
 
 from api.v1.views import app_views
-from flask import abort, request
+from flask import abort, jsonify, request
 from models.city import City
 from models import storage
 
