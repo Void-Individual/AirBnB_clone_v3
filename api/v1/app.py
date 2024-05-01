c14a3b57e521b18ca752e2432073b7e7e20dc699
Void-Individual <otayomitv@gmail.com>
2024-05-02 00:11:10 +0100

    added CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,10 @@ from os import getenv
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
