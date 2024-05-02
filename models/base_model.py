c2f23310fa6e39f9beed95f1a2ed285d9386225c
Void-Individual <otayomitv@gmail.com>
2024-05-02 01:25:46 +0100

    encoded password

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
+import hashlib
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -68,8 +69,14 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+
         return new_dict
 
     def delete(self):
         """delete the current instance from the storage"""
         models.storage.delete(self)
+
+class User(BaseModel):
+    def __init__(self, password, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.password = hashlib.md5(password.encode()).hexdigest()
