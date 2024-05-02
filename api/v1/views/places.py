7f56a63fa97e4e1caefb08374e2e90c42f3469a1
Void-Individual <otayomitv@gmail.com>
2024-05-02 04:12:37 +0100

    Repurposd the search to use the flask app call on itself for amenity

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,9 +6,12 @@ from models.city import City
 from models.place import Place
 from models.user import User
 from models.state import State
+from models.amenity import Amenity
 from flask import abort, jsonify, request
 from api.v1.views import app_views
-
+import requests
+import json
+from os import getenv
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
@@ -35,39 +38,55 @@ def places_search():
     data = request.get_json()
     if data is None:
         abort(400, 'Not a JSON')
+
     saved_places = storage.all(Place).values()
-    saved_cities = storage.all(City).values()
-    if data is None or (not data['states'] and not data['cities']
-                         and not data['amenities']):
+    if not data or (not data.get('states') and not data.get('cities')
+                         and not data.get('amenitites')):
         return jsonify([place.to_dict() for place in saved_places])
 
     matched_places = []
     if data.get('states'):
-        state_ids = data['states']
-        cities_in_states = [city for city in saved_cities if city.state_id in state_ids]
-        city_ids = [city.id for city in cities_in_states]
-        state_places = [place for place in saved_places if place.city_id in city_ids]
-        matched_places = state_places
+        states = [storage.get(State, id) for id in data.get('states')]
+        for state in states:
+            for city in state.cities:
+                for place in city.places():
+                    matched_places.append(place)
 
     if data.get('cities'):
-        city_ids = data['cities']
-        city_places = [place for place in saved_places if place.city_id in city_ids]
+        cities = [storage.get(City, id) for id in data.get('cities')]
+        for city in cities:
+            for place in city.places:
+                if place not in matched_places:
+                    matched_places.append(place)
 
-    for place in city_places:
-        if place not in matched_places:
-            matched_places.append(place)
+    if not matched_places:
+        matched_places = saved_places
 
     if data.get('amenities'):
-        amenity_ids = data['amenities']
-        place_with_amenities = [place for place in saved_places if place.amenity_ids == amenity_ids]
-        if matched_places:
-            matching_places = [place.to_dict() for place in matched_places if place in place_with_amenities]
-        else:
-            matching_places = [place.to_dict() for place in place_with_amenities]
-        return jsonify(matching_places)
-    else:
-        matching_places = [place.to_dict() for place in matched_places]
-        return jsonify(matching_places)
+        amenities = [storage.get(Amenity, id) for id in data.get('amenities')]
+        x = 0
+        limit = len(amenities)
+        HBNB_API_HOST = getenv('HBNB_API_HOST')
+        HBNB_API_PORT = getenv('HBNB_API_PORT')
+
+        port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
+        first_url = "http://0.0.0.0:{}/api/v1/places/".format(port)
+
+        while x < limit:
+            place = matched_places[x]
+            url = first_url + '{}/amenities'
+            req = url.format(place.id)
+            response = requests.get(req)
+            resp = json.loads(response)
+            p_amenities = [storage.get(Amenity , obj[id]) for obj in resp]
+            for amenity in p_amenities:
+                if amenity not in amenities:
+                    matched_places.pop(x)
+                    x -= 1
+                    limit -= 1
+                    break
+            x += 1
+    return jsonify([place.to_dict() for place in matched_places])
 
 
 @app_views.route('/places/<id>', methods=['GET'], strict_slashes=False)
